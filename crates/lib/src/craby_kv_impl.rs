b4902864ca010e08b92610081d9c7bd17cec9032
leegeunhyeok <dev.ghlee@gmail.com>
2025-10-27 08:52:06 +0900

    feat: migrate to sled

--- crates/lib/src/craby_kv_impl.rs
+++ crates/lib/src/craby_kv_impl.rs
@@ -1,34 +1,26 @@
 use std::path::Path;
 
-use kv::{Bucket, Config, Raw, Store};
+use sled::Db;
 
 use crate::context::*;
 use crate::ffi::bridging::*;
 use crate::generated::*;
 use crate::types::*;
 
-const BUCKET_NAME: &str = "craby_kv";
-
 pub struct CrabyKv {
     ctx: Context,
-    bucket: Option<Bucket<'static, Raw, Raw>>,
+    db: Option<Db>,
 }
 
 impl CrabyKv {
-    fn get_bucket(&self) -> &Bucket<'static, Raw, Raw> {
-        self.bucket.as_ref().expect("Bucket not initialized")
-    }
-}
-
-impl Drop for CrabyKv {
-    fn drop(&mut self) {
-        self.bucket.take();
+    fn get_db(&self) -> &Db {
+        self.db.as_ref().expect("Database not initialized")
     }
 }
 
 impl CrabyKvSpec for CrabyKv {
     fn new(ctx: Context) -> Self {
-        CrabyKv { ctx, bucket: None }
+        CrabyKv { ctx, db: None }
     }
 
     fn id(&self) -> usize {
@@ -36,71 +28,53 @@ impl CrabyKvSpec for CrabyKv {
     }
 
     fn initialize(&mut self) -> Void {
-        let cfg = Config::new(Path::new(&self.ctx.data_path));
-        let store = Store::new(cfg).expect("Failed to create store");
-        self.bucket = Some(
-            store
-                .bucket(Some(BUCKET_NAME))
-                .expect("Failed to get bucket"),
+        self.db = Some(
+            sled::open(
+                Path::new(&self.ctx.data_path)
+                    .join("craby_sled")
+                    .to_string_lossy()
+                    .to_string(),
+            )
+            .expect("Failed to open database"),
         );
     }
 
     fn clear(&mut self) -> Void {
-        self.get_bucket().clear().expect("Failed to clear bucket");
+        self.get_db().clear().expect("Failed to clear database");
     }
 
     fn contains(&mut self, key: &str) -> Boolean {
-        self.get_bucket()
-            .contains(&key.to_raw())
+        self.get_db()
+            .contains_key(key)
             .expect("Failed to check if key exists")
     }
 
     fn get(&mut self, key: &str) -> Nullable<String> {
-        match self.get_bucket().get(&key.to_raw()) {
-            Ok(Some(value)) => {
-                Nullable::<String>::some(String::from_utf8_lossy(&value).to_string())
-            }
+        match self.get_db().get(key) {
+            Ok(Some(v)) => Nullable::<String>::some(String::from_utf8_lossy(&v).to_string()),
             Ok(None) => Nullable::<String>::none(),
             Err(e) => throw!("Failed to get value: {}", e.to_string()),
         }
     }
 
     fn keys(&mut self) -> Array<String> {
-        self.get_bucket()
+        self.get_db()
             .iter()
-            .map(|it| {
-                let key = it
-                    .expect("Failed to get item")
-                    .key::<Raw>()
-                    .expect("Failed to get key");
-                String::from_utf8_lossy(&key).to_string()
-            })
+            .map(|v| String::from_utf8_lossy(&v.expect("Failed to get item").0).to_string())
             .collect::<Array<String>>()
     }
 
     fn remove(&mut self, key: &str) -> Void {
-        self.get_bucket()
-            .remove(&key.to_raw())
-            .expect("Failed to remove key");
+        self.get_db().remove(key).expect("Failed to remove key");
     }
 
     fn set(&mut self, key: &str, value: &str) -> Void {
-        self.get_bucket()
-            .set(&key.to_raw(), &value.to_raw())
-            .expect("Failed to set value");
+        self.get_db()
+            .insert(key, value.as_bytes())
+            .expect("Failed to insert value");
     }
 
     fn size(&mut self) -> Number {
-        self.get_bucket().len() as Number
-    }
-}
-
-trait ToRaw {
-    fn to_raw(&self) -> Raw;
-}
-
-impl ToRaw for &str {
-    fn to_raw(&self) -> Raw {
-        Raw::from(self.as_bytes())
+        self.get_db().len() as Number
     }
 }
